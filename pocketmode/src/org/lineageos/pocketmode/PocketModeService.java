LineageOS/android_device_oneplus_msm8998-common

msm8998-common: pocketmode: Cleanup
- Remove unneeded stuff
- Update package name and tag
- Destroy class only after unregistering

Change-Id: I47ad0090ff61a2448bff2364f059504f0bc4deb3

pocketmode/src/org/lineageos/pocketmode/PocketModeService.java
@@ -1,3 +1,4 @@
+
 /*
  * Copyright (c) 2016 The CyanogenMod Project
  *
@@ -35,7 +36,8 @@ public class PocketModeService extends Service {
         if (DEBUG) Log.d(TAG, "Creating service");
         mPocketSensor = new PocketSensor(this);
 
-        IntentFilter screenStateFilter = new IntentFilter(Intent.ACTION_SCREEN_ON);
+        IntentFilter screenStateFilter = new IntentFilter();
+        screenStateFilter.addAction(Intent.ACTION_SCREEN_ON);
         screenStateFilter.addAction(Intent.ACTION_SCREEN_OFF);
         registerReceiver(mScreenStateReceiver, screenStateFilter);
     }
@@ -49,9 +51,9 @@ public class PocketModeService extends Service {
     @Override
     public void onDestroy() {
         if (DEBUG) Log.d(TAG, "Destroying service");
-        super.onDestroy();
         this.unregisterReceiver(mScreenStateReceiver);
         mPocketSensor.disable();
+        super.onDestroy();
     }
 
     @Override
@@ -59,23 +61,15 @@ public class PocketModeService extends Service {
         return null;
     }
 
-    private void onDisplayOn() {
-        if (DEBUG) Log.d(TAG, "Display on");
-        mPocketSensor.disable();
-    }
-
-    private void onDisplayOff() {
-        if (DEBUG) Log.d(TAG, "Display off");
-        mPocketSensor.enable();
-    }
-
     private BroadcastReceiver mScreenStateReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
             if (intent.getAction().equals(Intent.ACTION_SCREEN_ON)) {
-                onDisplayOn();
+                if (DEBUG) Log.d(TAG, "Display on");
+                mPocketSensor.disable();
             } else if (intent.getAction().equals(Intent.ACTION_SCREEN_OFF)) {
-                onDisplayOff();
+                if (DEBUG) Log.d(TAG, "Display off");
+                mPocketSensor.enable();
             }
         }
     };

ShapeShiftOS-Devices/device_oneplus_msm8998-common

pocketmode/src/org/lineageos/pocketmode/PocketModeService.java
@@ -25,6 +25,7 @@ import android.os.IBinder;
 import android.util.Log;
 
 public class PocketModeService extends Service {
+
     private static final String TAG = "PocketModeService";
     private static final boolean DEBUG = false;
 
@@ -49,9 +50,9 @@ public class PocketModeService extends Service {
     @Override
     public void onDestroy() {
         if (DEBUG) Log.d(TAG, "Destroying service");
-        super.onDestroy();
         this.unregisterReceiver(mScreenStateReceiver);
         mProximitySensor.disable();
+        super.onDestroy();
     }
 
     @Override
@@ -59,23 +60,15 @@ public class PocketModeService extends Service {
         return null;
     }
 
-    private void onDisplayOn() {
-        if (DEBUG) Log.d(TAG, "Display on");
-        mProximitySensor.disable();
-    }
-
-    private void onDisplayOff() {
-        if (DEBUG) Log.d(TAG, "Display off");
-        mProximitySensor.enable();
-    }
-
     private BroadcastReceiver mScreenStateReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
             if (intent.getAction().equals(Intent.ACTION_SCREEN_ON)) {
-                onDisplayOn();
+                if (DEBUG) Log.d(TAG, "Display on");
+                mProximitySensor.disable();
             } else if (intent.getAction().equals(Intent.ACTION_SCREEN_OFF)) {
-                onDisplayOff();
+                if (DEBUG) Log.d(TAG, "Display off");
+                mProximitySensor.enable();
             }
         }
     };